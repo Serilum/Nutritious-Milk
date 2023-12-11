c68507a3d30622202e9973ad6db3484f4f80a205
Rick South <rick@natamus.com>
2023-12-12 00:40:27 +0100

    Nutritious Milk source code for 1.19.2.

---
+++ Common/src/main/java/com/natamus/nutritiousmilk/events/MilkEvent.java
@@ -0,0 +1,23 @@
+package com.natamus.nutritiousmilk.events;
+
+import com.natamus.nutritiousmilk.config.ConfigHandler;
+import net.minecraft.core.Registry;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodData;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+public class MilkEvent {
+	public static void onDrink(Player player, ItemStack usedItem, ItemStack newItem, InteractionHand hand) {
+		Item item = usedItem.getItem();
+		String registryname = Registry.ITEM.getKey(item).toString();
+		if (item.equals(Items.MILK_BUCKET) || registryname.contains("milk_bucket")) {
+			FoodData fs = player.getFoodData();
+			
+			fs.setFoodLevel(fs.getFoodLevel() + ConfigHandler.hungerLevelIncrease);
+			fs.setSaturation(fs.getSaturationLevel() + (float)ConfigHandler.saturationLevelIncrease);
+		}
+	}
+}
