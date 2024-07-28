e97078cf26bfc2bf5f34205b7341c5f63798446b
Rick South <rick@natamus.com>
2024-07-28 07:23:14 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/nutritiousmilk/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/nutritiousmilk/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.nutritiousmilk;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.nutritiousmilk.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.nutritiousmilk.neoforge.events.NeoForgeMilkEvent;
 import com.natamus.nutritiousmilk.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
