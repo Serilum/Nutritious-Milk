46e2163a15db311d98664f018bc83d5ee85717a6
Rick South <rick@natamus.com>
2024-07-28 07:23:07 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/nutritiousmilk/util/Reference.java
+++ Common/src/main/java/com/natamus/nutritiousmilk/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.nutritiousmilk.util;
 public class Reference {
 	public static final String MOD_ID = "nutritiousmilk";
 	public static final String NAME = "Nutritious Milk";
-	public static final String VERSION = "3.3";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
