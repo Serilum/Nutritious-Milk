2859237b9acf162c920806fa41cc1df54371c0cd
Rick South <rick@natamus.com>
2024-08-09 08:13:55 +0200

    Nutritious Milk source code for 1.21.1.

---
+++ Fabric/src/main/java/com/natamus/nutritiousmilk/ModFabric.java
@@ -0,0 +1,36 @@
+package com.natamus.nutritiousmilk;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveItemEvents;
+import com.natamus.nutritiousmilk.events.MilkEvent;
+import com.natamus.nutritiousmilk.util.Reference;
+import net.fabricmc.api.ModInitializer;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveItemEvents.ON_ITEM_USE_FINISHED.register((player, usedItem, newItem, hand) -> {
+			MilkEvent.onDrink(player, usedItem, newItem, hand);
+			return null;
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
