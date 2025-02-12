fe2780996d99d452b720255e97878d95cb17ceff
Rick South <rick@natamus.com>
2025-02-12 23:23:18 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/nutritiousmilk/ModForge.java
+++ Forge/src/main/java/com/natamus/nutritiousmilk/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeMilkEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeMilkEvent.class);
 	}
 
 	private static void setGlobalConstants() {
--- Forge/src/main/java/com/natamus/nutritiousmilk/forge/events/ForgeMilkEvent.java
+++ Forge/src/main/java/com/natamus/nutritiousmilk/forge/events/ForgeMilkEvent.java
@@ -5,12 +5,10 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeMilkEvent {
 	@SubscribeEvent
-	public void onDrink(LivingEntityUseItemEvent.Finish e) {
+	public static void onDrink(LivingEntityUseItemEvent.Finish e) {
 		Entity entity = e.getEntity();
 		if (!(entity instanceof Player)) {
 			return;
