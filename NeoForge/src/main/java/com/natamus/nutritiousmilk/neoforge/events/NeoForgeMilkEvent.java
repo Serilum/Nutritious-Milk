09a2063a11884911dcddffedf55f129b01f3a4f1
Rick South <rick@natamus.com>
2025-02-12 23:23:26 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/nutritiousmilk/neoforge/events/NeoForgeMilkEvent.java
+++ NeoForge/src/main/java/com/natamus/nutritiousmilk/neoforge/events/NeoForgeMilkEvent.java
@@ -5,9 +5,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.event.entity.living.LivingEntityUseItemEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeMilkEvent {
 	@SubscribeEvent
 	public static void onDrink(LivingEntityUseItemEvent.Finish e) {
