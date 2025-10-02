eb249f44f8ae2703fd6485731f036242e398b9ea
Rick South <rick@natamus.com>
2025-10-02 12:29:35 +0200

    Nutritious Milk source code for 1.21.9.

---
+++ NeoForge/src/main/java/com/natamus/nutritiousmilk/neoforge/events/NeoForgeMilkEvent.java
@@ -0,0 +1,19 @@
+package com.natamus.nutritiousmilk.neoforge.events;
+
+import com.natamus.nutritiousmilk.events.MilkEvent;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.neoforge.event.entity.living.LivingEntityUseItemEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeMilkEvent {
+	@SubscribeEvent
+	public static void onDrink(LivingEntityUseItemEvent.Finish e) {
+		Entity entity = e.getEntity();
+		if (!(entity instanceof Player)) {
+			return;
+		}
+
+		MilkEvent.onDrink((Player)e.getEntity(), e.getItem(), null, null);
+	}
+}
