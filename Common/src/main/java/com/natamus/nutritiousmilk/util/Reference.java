f8efb105811a30e59d040430a67d96ee8387a638
Rick South <rick@natamus.com>
2026-06-18 23:36:36 +0200

    Updated Nutritious Milk to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/nutritiousmilk/util/Reference.java
+++ Common/src/main/java/com/natamus/nutritiousmilk/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "nutritiousmilk";
 	public static final String NAME = "Nutritious Milk";
 	public static final String VERSION = "3.5";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
